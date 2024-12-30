4e8616f1e47a9794a9fa410ff6784eae840a1355
Ahmed <ahmed.nabil1@nomadz.dev>
2024-12-30 20:02:27 +0200

    Added Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,20 @@ import 'package:evently_c13_online/core/theme/app_theme.dart';
 import 'package:evently_c13_online/ui/login/login_screen.dart';
 import 'package:evently_c13_online/ui/onbording/onboarding_setup_screen.dart';
 import 'package:evently_c13_online/ui/signup_screen/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyDf7oO7fF7We7ChS4aknvi8i-7fVSb1PiA",
+        appId: "1:361454085331:android:198f6916015a7d8d3b6026",
+        messagingSenderId: "",
+        projectId: "evently-mon-c13"),
+  );
   runApp(
     MultiProvider(providers: [
       ChangeNotifierProvider(
